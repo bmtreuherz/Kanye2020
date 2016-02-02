90d998065b8574dc3b6bc81b683c1eecdae6707b
Bradley Treuherz <bradleymtreuherz@gmail.com>
2016-02-02 13:26:53 -0500

    Started map XML parser

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,13 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.awt.*;
+import java.io.File;
 
 /**
  * Created by Bradley on 2/1/16.
@@ -10,7 +19,7 @@ public class Map {
     private final int MAP_HEIGHT = 50;
 
     public Map(){
-        initMap();
+        initMapXML();
     }
 
     private void initMap(){
@@ -34,6 +43,44 @@ public class Map {
                 // For now there are no area effects or items or entities.
                 tiles[i][j] = new Tile(terrain, areaEffect, item, entity);
             }
+        }
+    }
+
+    private void initMapXML(){
+        try{
+            // Create a document from the xml file
+            // TODO: Use relative path
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(new File(".\\src\\res\\maps\\default_map.xml"));
+
+            // Normalize
+            doc.getDocumentElement().normalize();
+
+            // Get the width and height of the map
+            int mapWidth = Integer.parseInt(doc.getDocumentElement().getAttribute("width"));
+            int mapHeight = Integer.parseInt(doc.getDocumentElement().getAttribute("height"));
+
+            NodeList rows = doc.getElementsByTagName("row");
+            System.out.println("NUM ROWS: " + rows.getLength());
+
+            for(int i=0; i<rows.getLength(); i++){
+                Element row = (Element) rows.item(i);
+                NodeList tiles = row.getElementsByTagName("tile");
+
+                for(int j=0; j<tiles.getLength(); j++){
+                    Element tile = (Element) tiles.item(j);
+                }
+
+            }
+            
+
+
+        }catch(SAXParseException e){
+            System.out.println("Error parsing");
+            e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
@@ -76,4 +123,10 @@ public class Map {
     public int getMapHeight(){
         return MAP_HEIGHT;
     }
+
+    //TODO: Remove this after debugging
+    public static void main(String[] args){
+        Map m = new Map();
+    }
+
 }
